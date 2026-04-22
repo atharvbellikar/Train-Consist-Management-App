2bbdc14b581920054570d0b16c82252d4787fe17
atharvbellikar <studiesfor43@gmail.com>
2026-04-22 10:08:02 +0530

    Submission snapshot for feature/UC2

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        // Create ArrayList for passenger bogies
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+
+        passengerBogies.remove("AC Chair");
+
+
+        System.out.println("\nPassenger Bogies after removal of AC Chair:");
+        System.out.println(passengerBogies);
+
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        } else {
+            System.out.println("\nSleeper bogie does not exist in the train.");
+        }
+
+        System.out.println("\nFinal Passenger Bogies:");
+        System.out.println(passengerBogies);
+    }
+}
