ee4a2d01e5f565f631745e7ce3f18a8e9099377a
atharvbellikar <studiesfor43@gmail.com>
2026-04-22 10:08:18 +0530

    Submission snapshot for feature/UC5

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,19 @@
+import java.util.LinkedHashSet;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+
+        trainFormation.add("Sleeper");
+
+        System.out.println("Final Train Formation (Insertion Order Preserved):");
+        System.out.println(trainFormation);
+    }
+}
